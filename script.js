0877eeaf5e01878c3dd825be866572431175aca9
Karyssa Miller <karyssakmiller@gmail.com>
2024-09-10 02:40:21 -0400

    Updated My Career and Added projects to my projects- need to format better 9/10/24 @2:40pm EST

--- script.js
+++ script.js
@@ -19,16 +19,14 @@ readMoreButtons.forEach(btn => {
     });
 });
 
-// const parentContainer = document.querySelector('.read-more-container');
-// parentContainer.addEventListener('click', event=>{
-//     const current = event.target;
-//     const isReadMoreBtn = current.className.includes('read-more-btn');
-
-//     if(!isReadMoreBtn) return;
-
-//     const currentText = event.target.parentNode.querySelector('.read-more-text');
-
-//     currentText.classList.toggle('read-more-text--show');
-
-//     current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
-// })
+document.addEventListener('DOMContentLoaded', function () {
+    const readMoreBtns = document.querySelectorAll('.read-more-btn');
+    
+    readMoreBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            const readMoreText = this.previousElementSibling;
+            readMoreText.classList.toggle('read-more-text--show');
+            this.textContent = readMoreText.classList.contains('read-more-text--show') ? 'Read Less...' : 'Read More...';
+        });
+    });
+});
